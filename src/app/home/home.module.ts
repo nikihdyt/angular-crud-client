0897e8800a4352b3e47d5eb13f808666a1be1e76
nikihdyt <nikihidayati20@gmail.com>
2023-07-17 16:32:37 +0700

    remove unused component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { StudentsComponent } from './students/students.component';
 import { ViewStudentsComponent } from './view-students/view-students.component';
 import { CreateStudentsComponent } from './create-students/create-students.component';
 import { UpdateStudentsComponent } from './update-students/update-students.component';
 
 @NgModule({
   declarations: [
-    StudentsComponent,
     ViewStudentsComponent,
     CreateStudentsComponent,
     UpdateStudentsComponent
@@ -21,7 +19,6 @@ import { UpdateStudentsComponent } from './update-students/update-students.compo
     ReactiveFormsModule
   ],
   exports: [
-    StudentsComponent,
     ViewStudentsComponent,
     CreateStudentsComponent,
     UpdateStudentsComponent
